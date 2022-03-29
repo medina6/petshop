537f33410b438f03f8167394aa1bc7eed89b07d3
medina6 <medinakubanych@gmail.com>
2022-03-29 16:25:36 +0600

    petshop

---
+++ order/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from pet.models import Animals
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Animals, on_delete=models.CASCADE, related_name='orders')
+    phone = models.CharField(max_length=13)
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.email} - {self.phone}'
+
+
+# from django.db import models
+#
+# from main.models import Book
+#
+#
+# class Order(models.Model):
+#     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='orders')
+#     phone = models.CharField(max_length=13)
+#     address = models.TextField()
+#     city = models.CharField(max_length=100)
+#     email = models.EmailField()
+#
+#     def __str__(self):
+#         return f'{self.email} - {self.phone}'
+
+
+
---
+++ pet/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from pet.models import *
+
+admin.site.register(Animals)
+admin.site.register(Product)
+
